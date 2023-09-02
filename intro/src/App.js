e768da9b05a31b1c74ec61e96e777c1b53b18c53
Yavuz Yilmaz <74655562+YavuzYilmazz@users.noreply.github.com>
2023-09-02 16:15:25 +0300

    feat(intro): create Columns and edit Categorylist

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import Navi from './Navi';
-import CategoryList from './CategoryList';
-import ProductList from './ProductList';
-
+import React from 'react'
+import Navi from './Navi'
+import CategoryList from './CategoryList'
+import ProductList from './ProductList'
+import { Container, Row, Col } from 'reactstrap'
 
 function App() {
   return (
-    <div className="App">
-      <Navi/>
-      <CategoryList/>
-      <ProductList/>
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Navi />
+          </Col>
+        </Row>
+        <Row>
+          <Col md='3'>
+            <CategoryList />
+          </Col>
+          <Col md='9'>
+            <ProductList /> 
+          </Col>
+        </Row>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
   render() {
     return (
       <div>
         <h3>Category List</h3>
+        <ListGroup> 
+          <ListGroupItem>Category 1</ListGroupItem>
+          <ListGroupItem>Category 2</ListGroupItem>
+          <ListGroupItem>Category 3</ListGroupItem>
+          <ListGroupItem>Category 4</ListGroupItem>
+          <ListGroupItem>Category 5</ListGroupItem>
+        </ListGroup>
       </div>
     )
   }
