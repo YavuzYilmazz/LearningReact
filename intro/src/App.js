a2816375e40b582c0586174ff1ebfd532d6293c2
Yavuz Yilmaz <74655562+YavuzYilmazz@users.noreply.github.com>
2023-09-03 12:50:49 +0300

    feat(intro): using event

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,32 +1,38 @@
-import React from 'react'
-import Navi from './Navi'
-import CategoryList from './CategoryList'
-import ProductList from './ProductList'
-import { Container, Row, Col } from 'reactstrap'
+import React from "react";
+import Navi from "./Navi";
+import { Component } from "react";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  let CategoryInfo = { title: "Category List" }
-  let ProductInfo = { title: "Product List" }
+export default class App extends Component {
+state={currentCategory:""}
 
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <Navi />
-          </Col>
-        </Row>
-        <Row>
-          <Col md='3'>
-            <CategoryList info={CategoryInfo}/>
-          </Col>
-          <Col md='9'>
-            <ProductList info={ProductInfo}/> 
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  )
+changeCategory = (category) => {
+  this.setState({currentCategory:category.CategoryName})
+  }
+
+  render() {
+    let CategoryInfo = { title: "Category List" };
+    let ProductInfo = { title: "Product List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <Navi />
+            </Col>
+          </Row>
+          <Row>
+            <Col md="3">
+              <CategoryList   currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={CategoryInfo} />
+            </Col>
+            <Col md="9">
+              <ProductList info={ProductInfo} currentCategory={this.state.currentCategory} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App
