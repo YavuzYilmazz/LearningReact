29a08a83ca97ef63540a3be9680db9453be84e7d
Yavuz Yilmaz <74655562+YavuzYilmazz@users.noreply.github.com>
2023-09-03 12:12:58 +0300

    feat(intro): add state

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  state = {
+    categories: [
+      { CategoryId: 1, CategoryName: "Beverages" },
+      { CategoryId: 2, CategoryName: "Foods" },
+    ],
+  };
 
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
-        <ListGroup> 
-          <ListGroupItem>Category 1</ListGroupItem>
-          <ListGroupItem>Category 2</ListGroupItem>
-          <ListGroupItem>Category 3</ListGroupItem>
-          <ListGroupItem>Category 4</ListGroupItem>
-          <ListGroupItem>Category 5</ListGroupItem>
+        <ListGroup>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.CategoryId}>
+              {category.CategoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
       </div>
-    )
+    );
   }
 }
