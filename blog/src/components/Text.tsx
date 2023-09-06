8bb76e30af48f6d8bd343f8968973885ea9ee42c
Yavuz Yilmaz <74655562+YavuzYilmazz@users.noreply.github.com>
2023-09-06 13:28:51 +0300

    feat(blog):delete

--- blog/src/components/Text.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./../Ui/Text.css";
-
-export default class Text extends Component {
-  render() {
-    return <div>Just A Short Break</div>;
-  }
-}
