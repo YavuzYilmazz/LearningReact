4c31180ea916565a029cfcf46da56920069fea7e
Yavuz Yilmaz <74655562+YavuzYilmazz@users.noreply.github.com>
2023-09-06 13:12:21 +0300

    feat(blog):edit

--- blog/src/components/Footer.tsx
+++ blog/src/components/Footer.tsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./../Ui/Footer.css";
 
 export default class Footer extends Component {
   render() {
     return (
       <footer className="footer-container">
-      <p className="p">Developed by Yavuz Yılmaz</p>
-      <a href="https://www.linkedin.com/in/yavuz-yilmazz/" target="_blank" rel="noopener noreferrer">
-        <button className="footer-button">Go to Linkedin</button>
-      </a>
-    </footer>
+        <p className="p">Developed by Yavuz Yılmaz</p>
+        <a
+          href="https://www.linkedin.com/in/yavuz-yilmazz/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="footer-button">Go to Linkedin</button>
+        </a>
+      </footer>
     );
   }
 }
--- blog/src/components/Text.tsx
+++ blog/src/components/Text.tsx
@@ -1,11 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./../Ui/Text.css";
-
 
 export default class Text extends Component {
   render() {
-    return (
-      <div>Just A Short Break</div>
-    )
+    return <div>Just A Short Break</div>;
   }
 }
